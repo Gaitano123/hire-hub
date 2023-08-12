3d455c8fcd8d81be873a6a471f921891557375b8
Gaitano <gaitano.george@student.moringaschool.com>
2023-08-12 12:46:52 +0300

    Added build

--- src/Components/JOBS/Filtered-Country/Kenyan.js
+++ src/Components/JOBS/Filtered-Country/Kenyan.js
@@ -10,7 +10,6 @@ function Kenyan({ jobs }){
 
 
     useEffect(() => {
-        // Scroll to the top of the page when the component mounts
         window.scrollTo(0, 0);
 
         setTimeout(() => {
